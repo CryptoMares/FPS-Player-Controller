a52c8f456fc49f8897e408cd95168f6eedd5041a
CryptoMares <ivanvodko@ya.ru>
2024-10-17 16:18:42 +0300

    stairs: working but bugged af

--- scripts/fps_controller.gd
+++ scripts/fps_controller.gd
@@ -57,21 +57,22 @@ func _handle_camera_rotation(event: InputEvent):
 	var new_head_rot_x = clamp(head_rot_x, deg_to_rad(-90) - (head_rot_x - deg_to_rad(60)) * 0.1, deg_to_rad(60) + (head_rot_x - deg_to_rad(-80)) * 0.1)
 	$Head.rotation.x = lerp($Head.rotation.x, new_head_rot_x, 0.35)
 
-
+# Stair functions START
 func is_surface_too_steep(normal : Vector3) -> bool:
 	return normal.angle_to(Vector3.UP) > self.floor_max_angle
 
 func _run_body_test_motion(from : Transform3D, motion : Vector3, result = null) -> bool:
 	if not result: result = PhysicsTestMotionResult3D.new()
-	var params = PhysicsTestMotionResult3D.new()
+	var params = PhysicsTestMotionParameters3D.new()
 	params.from = from
 	params.motion = motion
 	return PhysicsServer3D.body_test_motion(self.get_rid(), params, result)
 
-func snap_down_to_stairs_check() -> void:
+func _snap_down_to_stairs_check() -> void:
 	var did_snap := false
+	var floor_below : bool = %StairsBelowRayCast3D.is_colliding() and not is_surface_too_steep(%StairsBelowRayCast3D.get_collision_normal())
 	var was_on_floor_last_frame = Engine.get_physics_frames() - _last_frame_was_on_floor == 1
-	if not is_on_floor() and velocity.y <= 0 and (was_on_floor_last_frame or _snapped_to_stairs_last_frame):
+	if not is_on_floor() and velocity.y <= 0 and (was_on_floor_last_frame or _snapped_to_stairs_last_frame) and floor_below:
 		var body_test_result = PhysicsTestMotionResult3D.new()
 		if _run_body_test_motion(self.global_transform, Vector3(0, -MAX_STEP_HEIGHT, 0), body_test_result):
 			var translate_y = body_test_result.get_travel().y
@@ -80,6 +81,25 @@ func snap_down_to_stairs_check() -> void:
 			did_snap = true
 	_snapped_to_stairs_last_frame = did_snap
 
+func _snap_up_stairs_check(delta) -> bool:
+	if not is_on_floor() and not _snapped_to_stairs_last_frame: return false
+	var expected_move_motion = self.velocity + Vector3(1,0,1) * delta
+	var step_pos_with_clearance = self.global_transform.translated(expected_move_motion + Vector3(0, MAX_STEP_HEIGHT * 2, 0))
+	var down_check_result = PhysicsTestMotionResult3D.new()
+	if (_run_body_test_motion(step_pos_with_clearance, Vector3(0,-MAX_STEP_HEIGHT*2,0), down_check_result) 
+	and (down_check_result.get_collider().is_class("StaticBody3D") or down_check_result.get_collider().is_class("CSGShape3D"))):
+		var step_height = ((step_pos_with_clearance.origin + down_check_result.get_travel()) - self.global_position).y
+		if step_height > MAX_STEP_HEIGHT or step_height <= 0.01 or (down_check_result.get_collision_point() - self.global_position).y > MAX_STEP_HEIGHT: return false
+		%StairsAheadRayCast3D.global_position = down_check_result.get_collision_point() + Vector3(0,MAX_STEP_HEIGHT,0) + expected_move_motion.normalized() * 0.1
+		%StairsAheadRayCast3D.force_raycast_update()
+		if %StairsAheadRayCast3D.is_colliding() and not is_surface_too_steep(%StairsAheadRayCast3D.get_collision_normal()):
+			self.global_position = step_pos_with_clearance.origin + down_check_result.get_travel()
+			apply_floor_snap()
+			_snapped_to_stairs_last_frame = true
+			return true
+	return false
+
+# Stair functions END
 
 func _physics_process(delta):
 	process_input()
@@ -131,8 +151,9 @@ func process_movement(delta):
 		velocity = update_velocity_air(wish_dir, delta)
 
 	# Move the player once velocity has been calculated
-	move_and_slide()
-	snap_down_to_stairs_check()w
+	if not _snap_up_stairs_check(delta):
+		move_and_slide()
+		_snap_down_to_stairs_check()
 
 func accelerate(wish_dir: Vector3, max_velocity: float, delta):
 	# Get our current speed as a projection of velocity onto the wish_dir
